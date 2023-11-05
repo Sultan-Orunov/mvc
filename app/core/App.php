55985eaac7f9e42276f3db385a7a367ffb2ca25f
Sultan <osbfswd@gmail.com>
2023-11-05 21:38:46 +0500

    added the core folder

---
+++ app/core/App.php
@@ -0,0 +1,22 @@
+<?php
+
+class App
+{
+  private function splitURL()
+  {
+    $url = $_GET['url'] ? $_GET['url'] : 'home';
+    $url = explode('/', $url);
+    return $url;
+  }
+
+  public function loadController()
+  {
+    $url = $this->splitURL();
+    $fileName = '../app/controllers/' . ucfirst($url[0]) . '.php';
+    if (file_exists($fileName)) {
+      require $fileName;
+    } else {
+      require '../app/controllers/_404.php';
+    }
+  }
+}
--- public/index.php
+++ public/index.php
@@ -1,29 +1,5 @@
 <?php
-session_start();
-
-require_once '../app/core/functions.php';
-
-class App
-{
-  private function splitURL()
-  {
-    $url = $_GET['url'] ? $_GET['url'] : 'home';
-    $url = explode('/', $url);
-    return $url;
-  }
-
-  public function loadController()
-  {
-    $url = $this->splitURL();
-    $fileName = '../app/controllers/' . ucfirst($url[0]) . '.php';
-    if (file_exists($fileName)) {
-      require $fileName;
-    } else {
-      require '../app/controllers/_404.php';
-    }
-  }
-}
-
+require_once '../app/core/init.php';
 
 
 $app = new App();
