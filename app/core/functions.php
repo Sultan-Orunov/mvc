44690fc9dd56f69ba11ee964a5303224a2987b57
Sultan <osbfswd@gmail.com>
2023-11-09 21:53:32 +0500

    добавил функции которые отображают данные после перезагрузки

--- app/core/functions.php
+++ app/core/functions.php
@@ -109,3 +109,48 @@ function message(string $msg = null, bool $clear = false)
 
   return false;
 }
+
+/** отображает введенные данные после перезагрузки страницы */
+function old_checked(string $key, string $value, string $default = ""): string
+{
+
+  if (isset($_POST[$key])) {
+    if ($_POST[$key] == $value) {
+      return ' checked ';
+    }
+  } else {
+
+    if ($_SERVER['REQUEST_METHOD'] == "GET" && $default == $value) {
+      return ' checked ';
+    }
+  }
+
+  return '';
+}
+
+
+function old_value(string $key, mixed $default = "", string $mode = 'post'): mixed
+{
+  $POST = ($mode == 'post') ? $_POST : $_GET;
+  if (isset($POST[$key])) {
+    return $POST[$key];
+  }
+
+  return $default;
+}
+
+function old_select(string $key, mixed $value, mixed $default = "", string $mode = 'post'): mixed
+{
+  $POST = ($mode == 'post') ? $_POST : $_GET;
+  if (isset($POST[$key])) {
+    if ($POST[$key] == $value) {
+      return " selected ";
+    }
+  } else
+
+  if ($default == $value) {
+    return " selected ";
+  }
+
+  return "";
+}
