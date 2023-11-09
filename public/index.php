ea112dd2c3a96971383b1e16561c6e2bcd99db1c
Sultan <osbfswd@gmail.com>
2023-11-09 21:07:11 +0500

    добавил проверку версии PHP

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 require_once '../app/core/init.php';
+$minPHPVersion = '8.0';
+if (phpversion() < $minPHPVersion) {
+  die("Для запускаа этого приложения ваша версия PHP должна быть {$minPHPVersion} или выше. Ваша текущая версия " . phpversion());
+}
 
 DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
 
