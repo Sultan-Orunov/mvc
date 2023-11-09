38db2987c4b67052554e841be2baedee37fe0a69
Sultan <osbfswd@gmail.com>
2023-11-09 21:26:20 +0500

    Проверка PHP расширений

--- app/core/functions.php
+++ app/core/functions.php
@@ -1,5 +1,33 @@
 <?php
 
+/** check which php extensions are required */
+check_extensions();
+function check_extensions()
+{
+
+  $required_extensions = [
+
+    'gd',
+    'pdo_mysql',
+    'mbstring',
+    'exif',
+  ];
+
+  $not_loaded = [];
+
+  foreach ($required_extensions as $ext) {
+
+    if (!extension_loaded($ext)) {
+      $not_loaded[] = $ext;
+    }
+  }
+
+  if (!empty($not_loaded)) {
+    show("Please load the following extensions in your php.ini file: <br>" . implode("<br>", $not_loaded));
+    die;
+  }
+}
+
 function show($stuff)
 {
   echo "<pre>";
@@ -19,5 +47,16 @@ function dd($stuff)
   echo "<pre>";
   var_dump($stuff);
   echo "</pre>";
+  die();
+}
+
+function esc($str)
+{
+  return htmlspecialchars($str);
+}
+
+function redirect($path)
+{
+  header("Location: " . ROOT . "/" . $path);
   die();
 }
